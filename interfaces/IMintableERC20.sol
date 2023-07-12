c6562033588344643afeb148d8da32bccad419fe
1drien <adrienmorlet2@gmail.com>
2023-07-12 18:46:34 +0200

    add interfaces files

---
+++ interfaces/IMintableERC20.sol
@@ -0,0 +1,63 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+interface IMintableERC20 {
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+    event OwnershipTransferred(
+        address indexed previousOwner,
+        address indexed newOwner
+    );
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function burn(address account, uint256 amount) external;
+
+    function decimals() external view returns (uint8);
+
+    function decreaseAllowance(
+        address spender,
+        uint256 subtractedValue
+    ) external returns (bool);
+
+    function increaseAllowance(
+        address spender,
+        uint256 addedValue
+    ) external returns (bool);
+
+    function mint(address account, uint256 amount) external;
+
+    function name() external view returns (string memory);
+
+    function owner() external view returns (address);
+
+    function renounceOwnership() external;
+
+    function symbol() external view returns (string memory);
+
+    function totalSupply() external view returns (uint256);
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    function transferOwnership(address newOwner) external;
+}
---
+++ interfaces/IVeQi.sol
@@ -0,0 +1,20 @@
+import "./IVeERC20.sol";
+
+/**
+ * @dev Interface of the VeQi
+ */
+interface IVeQi is IVeERC20 {
+    function isUser(address _addr) external view returns (bool);
+
+    function deposit(uint256 _amount) external;
+
+    function claim() external;
+
+    function withdraw(uint256 _amount) external;
+
+    function getStakedQi(address _addr) external view returns (uint256);
+
+    function eventualTotalSupply() external view returns (uint256);
+
+    function eventualBalanceOf(address account) external view returns (uint256);
+}
