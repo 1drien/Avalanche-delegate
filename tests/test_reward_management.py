81f9f36f884e04d0e22e954c9d80d6c918017089
Nils Joanne <nils_joanne@yahoo.com>
2023-07-21 12:35:25 +0200

    Add test where only Mainstaking can call claim for reward (work in progress

--- tests/test_reward_management.py
+++ tests/test_reward_management.py
@@ -15,7 +15,7 @@ def test_xqi_required():
     xqi = xQI.deploy(qi.address, veqi.address,parameter)
 
     ### Sets amount as the allowance of spender over the owner's tokens
-    qi.approve(xqi.address, amount, {"from": user})
+    qi.approve(xqi.address, amount, parameter)
 
     ### Stakes QI from user on xQI contract
     xqi.depositQI(amount, user, parameter)
@@ -30,6 +30,26 @@ def test_xqi_required():
     approval = event_data[0]['isApproved']
     assert value > 0, "Balance must be strictly positive"
     assert approval == True
+
+def test_call_claim():
+    user = accounts.at("0x142eb2ed775e6d497aa8d03a2151d016bbfe7fc2", True)
+    parameter = {"from": user}
+    amount = Wei("10 ether")
+
+    qi = interface.IMintableERC20("0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5")
+    veqi = interface.IMintableERC20("0x7Ee65Fdc1C534A6b4f9ea2Cc3ca9aC8d6c602aBd")
+    xqi = xQI.deploy(qi.address, veqi.address,parameter)
+
+    qi.approve(xqi.address, amount, parameter)
+
+    xqi.depositQI(amount, user, parameter)
+    user_balance = xqi.balanceOf(user)
+
+    mainstaking = MainStaking.deploy(xqi.address, parameter)
+
+    ### Test if only MainStaking can call claim for reward
+
+
 
 
 
