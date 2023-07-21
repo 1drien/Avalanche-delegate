f146f67d7a94feba49631fa2d2c114025f1a0b7b
1drien <adrienmorlet2@gmail.com>
2023-07-21 17:31:00 +0200

    adding new fixtures in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,16 @@
 import pytest
+from brownie import (
+    MainStaking,
+    BaseRewardPool,
+    Qi,
+    interface,
+    accounts,
+    Wei,
+    Contract,
+    chain,
+    BaseRewardPool,
+    xQI,
+)
 
 
 @pytest.fixture(autouse=True)
@@ -6,3 +18,20 @@ def isolation(
     fn_isolation,
 ):  # TO BE REPLACED BY py_vector.common.testing simple_isolation if issues
     pass
+
+
+@pytest.fixture
+def setup_contracts(accounts, interface, xQI, MainStaking):
+    user = accounts.at("0x142eb2ed775e6d497aa8d03a2151d016bbfe7fc2", True)
+    parameter = {"from": user}
+    amount = Wei("10 ether")
+
+    qi = interface.IMintableERC20("0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5")
+    veqi = interface.IMintableERC20("0x7Ee65Fdc1C534A6b4f9ea2Cc3ca9aC8d6c602aBd")
+    xqi = xQI.deploy(qi.address, veqi.address, parameter)
+
+    qi.approve(xqi.address, amount, parameter)
+
+    mainstaking = MainStaking.deploy(xqi.address, parameter)
+
+    return qi, xqi, mainstaking, user, amount
