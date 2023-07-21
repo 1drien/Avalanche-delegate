09df46958c0f2e350d2309843f8fe53d734a67b5
Nils Joanne <nils_joanne@yahoo.com>
2023-07-21 12:11:19 +0200

    Uses the ClaimApproved event for assertion and the code is cleaner

--- tests/test_reward_management.py
+++ tests/test_reward_management.py
@@ -4,32 +4,36 @@ from brownie import xQI, Qi, interface, accounts, Wei, Contract, chain, BaseRewa
 
 
 def test_xqi_required():
-    ### User with 0 qi
-    user1 = accounts[0]
-    parameter = {'from':user1}
-    
-    ### user1 with qi
-    user12 = accounts.at("0x142eb2ed775e6d497aa8d03a2151d016bbfe7fc2", True)
-    user12_parameters = {"from": user12}
-    qi = interface.IMintableERC20("0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5")
-   
-    ### Transfers amount from user2 to user1
+
+    ### user with qi
+    user = accounts.at("0x142eb2ed775e6d497aa8d03a2151d016bbfe7fc2", True)
+    parameter = {"from": user}
     amount = Wei("10 ether")
-    qi.transfer(user1, amount, user12_parameters)
 
+    qi = interface.IMintableERC20("0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5")
     veqi = interface.IMintableERC20("0x7Ee65Fdc1C534A6b4f9ea2Cc3ca9aC8d6c602aBd")
-    xqi = xQI.deploy(qi.address, veqi.address, {"from": user1})
+    xqi = xQI.deploy(qi.address, veqi.address,parameter)
 
-    qi.approve(xqi.address, amount, {"from": user1})
+    ### Sets amount as the allowance of spender over the owner's tokens
+    qi.approve(xqi.address, amount, {"from": user})
 
-    xqi.depositQI(amount, user1, {"from": user1})
-    user1_balance = xqi.balanceOf(user1)
-    # avax address : 0x1ce0c2827e2ef14d5c4f29a091d735a204794041
+    ### Stakes QI from user on xQI contract
+    xqi.depositQI(amount, user, parameter)
+    user_balance = xqi.balanceOf(user)
 
     mainstaking = MainStaking.deploy(xqi.address, parameter)
     
-    ### Test if the require statement passed successfully
-    # with pytest.raises(brownie.exceptions.VirtualMachineError):
-    #     mainstaking.claimApprove(user1_balance, parameter)
+    ### Test if the require statement passed successfully through the event
+    tx = mainstaking.claimApprove(user_balance)
+    event_data = tx.events['ClaimApproved']
+    value = event_data[0]['amount']
+    approval = event_data[0]['isApproved']
+    assert value > 0, "Balance must be strictly positive"
+    assert approval == True
+
+
+
+
+
 
 
