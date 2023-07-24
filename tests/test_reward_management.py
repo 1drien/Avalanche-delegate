14eb99e3a6652774d82dc12658f63bee76fe9934
1drien <adrienmorlet2@gmail.com>
2023-07-24 10:46:12 +0200

    added additional tests

--- tests/test_reward_management.py
+++ tests/test_reward_management.py
@@ -1,59 +1,46 @@
 import pytest
 import brownie
-from brownie import xQI, Qi, interface, accounts, Wei, Contract, chain, BaseRewardPool, MainStaking
+from brownie import (
+    xQI,
+    Qi,
+    interface,
+    accounts,
+    Wei,
+    Contract,
+    chain,
+    BaseRewardPool,
+    MainStaking,
+)
 
 
-def test_xqi_required():
+def test_xqi_required(setup_contracts, fn_isolation):
+    qi, xqi, mainstaking, user, amount = setup_contracts
+    value = Wei("10 ether")
 
-    ### user with qi
-    user = accounts.at("0x142eb2ed775e6d497aa8d03a2151d016bbfe7fc2", True)
-    parameter = {"from": user}
-    amount = Wei("10 ether")
-
-    qi = interface.IMintableERC20("0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5")
-    veqi = interface.IMintableERC20("0x7Ee65Fdc1C534A6b4f9ea2Cc3ca9aC8d6c602aBd")
-    xqi = xQI.deploy(qi.address, veqi.address,parameter)
-
-    ### Sets amount as the allowance of spender over the owner's tokens
-    qi.approve(xqi.address, amount, parameter)
-
-    ### Stakes QI from user on xQI contract
-    xqi.depositQI(amount, user, parameter)
+    assert mainstaking.balanceOf(user) == value
+    xqi.depositQI(amount, user, {"from": user})
     user_balance = xqi.balanceOf(user)
 
-    mainstaking = MainStaking.deploy(xqi.address, parameter)
-    
     ### Test if the require statement passed successfully through the event
     tx = mainstaking.claimApprove(user_balance)
-    event_data = tx.events['ClaimApproved']
-    value = event_data[0]['amount']
-    approval = event_data[0]['isApproved']
+    event_data = tx.events["ClaimApproved"]
+    value = event_data[0]["amount"]
+    approval = event_data[0]["isApproved"]
     assert value > 0, "Balance must be strictly positive"
     assert approval == True
 
-def test_call_claim():
-    user = accounts.at("0x142eb2ed775e6d497aa8d03a2151d016bbfe7fc2", True)
-    parameter = {"from": user}
-    amount = Wei("10 ether")
 
-    qi = interface.IMintableERC20("0x8729438EB15e2C8B576fCc6AeCdA6A148776C0F5")
-    veqi = interface.IMintableERC20("0x7Ee65Fdc1C534A6b4f9ea2Cc3ca9aC8d6c602aBd")
-    xqi = xQI.deploy(qi.address, veqi.address,parameter)
+def test_call_claim(setup_contracts):
+    qi, xqi, mainstaking, user, _ = setup_contracts
+    other_user = accounts[1]
 
-    qi.approve(xqi.address, amount, parameter)
-
-    xqi.depositQI(amount, user, parameter)
-    user_balance = xqi.balanceOf(user)
-
-    mainstaking = MainStaking.deploy(xqi.address, parameter)
-
-    ### Test if only MainStaking can call claim for reward
+    with reverts("Only MainStaking can call this function"):
+        mainstaking.claim({"from": other_user})
 
 
+def test_call_claim_by_other(setup_contracts):
+    qi, xqi, mainstaking, user, _ = setup_contracts
+    other_user = accounts[1]
 
-
-
-
-
-
-
+    with reverts("Permission Denied"):
+        mainstaking.someAction({"from": other_user})
