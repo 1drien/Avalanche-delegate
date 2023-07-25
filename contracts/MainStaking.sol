9ce54791b86db952b62820b07437c168fbe9e36e
Nils Joanne <nils_joanne@yahoo.com>
2023-07-25 17:54:25 +0200

    Added MainStaking_init function + empty withdrawReward function

--- contracts/MainStaking.sol
+++ contracts/MainStaking.sol
@@ -17,22 +17,35 @@ contract MainStaking is Initializable {
     address public immutable QI;
     address public immutable VeQi;
 
-    constructor(address _QI, address _VeQi, address _xQI) {
+    constructor(address _QI, address _VeQi) {
         QI = _QI;
         VeQi = _VeQi;
+    }
+
+    event xQIMinted(address indexed user, uint256 xqi_amount);
+    event RewardWithdrawed(uint256 reward);
+
+
+    function __MainStaking_init(address _xQI) external {
         xqi = xQI(_xQI);
     }
 
-    event xQIMinted(address indexed user, uint256 amount);
 
     /// @notice Stake QI in VeQi protocol and mint xQI at a 1:1 rate
     /// @param _amount the amount of QI
-    function depositQI(uint256 _amount, address _for) external {
+    function depositQI(uint256 _amount) external {
         IERC20(QI).safeTransferFrom(msg.sender, address(this), _amount);
         IERC20(QI).approve(VeQi, _amount);
         IVeQi(VeQi).deposit(_amount);
-        xqi.mintToken(_for, _amount);
+        xqi.mintToken(_amount);
         xqi.transferToMainStaking(address(this), _amount);
-        emit xQIMinted(_for, _amount);
+        emit xQIMinted(msg.sender, _amount);
+    }
+
+
+    /// @notice User withdraws belonging rewards
+    function withdrawReward() external {
+        uint256 reward = 0;
+        emit RewardWithdrawed(reward);
     }
 }
